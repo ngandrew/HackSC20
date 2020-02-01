3a74661db44ab9af7abd796522fcfd2144c32daa
Timothy Poon <timothy.poon520@gmail.com>
2020-02-01 15:14:30 -0800

    add frontend

--- collegeDatabase/backend/main.py
+++ collegeDatabase/backend/main.py
@@ -5,21 +5,78 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Table, Column, String, MetaData
 from sqlalchemy.orm import sessionmaker
+import json
 
 from database import init_db
 from database import db_session
 
-from models import User, HelpText
+from models import User, HelpText, College
 
 init_db()
-app = Flask(__name__)
+app = Flask(__name__, static_folder="../frontend", static_url_path="")
 
 
 @app.route('/helpData')
 def helpData():
-    query = db_session.query(HelpText).filter_by(page=request.values.get('location'))
+    query = db_session.query(HelpText).filter_by(page=request.values.get("location"))
     res = db_session.execute(query).fetchone()[2]
     return jsonify(res)
+
+def get_average_price(cost_by_income, income):
+    if cost_by_income == {}:
+        return False
+
+    MAX_VAL = 999999999
+    min_range = MAX_VAL
+    price = 0
+    for k, v in cost_by_income.items():
+        lohi = k.split("-")
+        lo = int(lohi[0])
+        is_plus = lohi[1] == "plus"
+        if is_plus:
+            hi = MAX_VAL
+        else:
+            hi = int(lohi[1])
+        if lo <= income and (is_plus or income <= hi) and hi - lo < min_range:
+            price = v
+    return price
+
+@app.route('/colleges')
+def searchColleges():
+    data = json.loads(request.values.get("data"))
+    query = "SELECT * FROM colleges WHERE id IS NOT NULL "
+    is_public = True if request.values.get("publicPrivate") == "public" else False if data.get("publicPrivate") == "private" else None
+    if is_public != None:
+        query += "AND is_public is {} ".format(is_public)
+
+    query += "LIMIT 50"
+    res = []
+
+    for row in [{column: value for column, value in rowproxy.items()} for rowproxy in db_session.execute(query)]:
+        res.append({
+            "id": row["id"],
+            "name": row["name"],
+            "city": "Los Angeles",
+            "state": "CA",
+            "publicPrivate": "public" if row["is_public"] else "private",
+            "sat25": 800,
+            "sat75": 1500,
+            "act25": 28,
+            "act75": 35,
+            "expectedCost": get_average_price(row["cost_by_income"], int(data.get("income")) if data.get("income") else 50000),
+            "difficulty": "very difficult",
+            "calculatorLink": "http://" + row["price_calculator_url"],
+            "deadline": "1/23",
+            "website": "http://" + row["school_url"],
+        })
+
+    return jsonify(res)
+
+
+@app.route('/', defaults={'path': 'index.html'})
+@app.route('/<path:path>')
+def index(path):
+    return app.send_static_file(path)
 
 @app.after_request
 def after_request(response):
